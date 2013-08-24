6b7c593c99e679ad36845c166197b1f50048eb9e
John Landahl <john@landahl.org>
2013-08-24 15:36:44 -0400

    [COOK-3352] Move full path of repository_root attribute into attribute
    Signed-off-by: Seth Vargo <sethvargo@gmail.com>

--- attributes/default.rb
+++ attributes/default.rb
@@ -22,7 +22,7 @@
 
 default['maven']['version'] = 2
 default['maven']['m2_home'] = '/usr/local/maven'
-default['maven']['repository_root'] = '/usr/local'
+default['maven']['repository_root'] = '/usr/local/m2/repository'
 default['maven']['2']['version'] = "2.2.1"
 default['maven']['2']['url'] = "http://apache.mirrors.tds.net/maven/maven-2/#{node['maven']['2']['version']}/binaries/apache-maven-#{node['maven']['2']['version']}-bin.tar.gz"
 default['maven']['2']['checksum'] = "b9a36559486a862abfc7fb2064fd1429f20333caae95ac51215d06d72c02d376"
