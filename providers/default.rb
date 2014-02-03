01007da230a5d94b2bc9ba78d293ee06d5e170f2
Florin STAN <tom@tcnp3.com>
2014-02-03 12:18:31 -0700

    force the snapshot update

--- providers/default.rb
+++ providers/default.rb
@@ -30,13 +30,14 @@ def create_command_string(artifact_file, new_resource)
   artifact_id = '-DartifactId=' + new_resource.artifact_id
   version = '-Dversion=' + new_resource.version
   dest = '-Ddest=' + artifact_file
+  force_update = new_resource.version =~ /-SNAPSHOT$/? '-U':''
   repos = '-DremoteRepositories=' + new_resource.repositories.join(',')
   packaging = '-Dpackaging=' + new_resource.packaging
   classifier = '-Dclassifier=' + new_resource.classifier if new_resource.classifier
   plugin_version = '2.4'
   plugin = "org.apache.maven.plugins:maven-dependency-plugin:#{plugin_version}:get"
   transitive = '-Dtransitive=' + new_resource.transitive.to_s
-  %Q{mvn #{plugin} #{group_id} #{artifact_id} #{version} #{packaging} #{classifier} #{dest} #{repos} #{transitive}}
+  %Q{mvn #{force_update} #{plugin} #{group_id} #{artifact_id} #{version} #{packaging} #{classifier} #{dest} #{repos} #{transitive}}
 end
 
 def get_mvn_artifact(action, new_resource)
