1ae5ff4ae6e3d52ee7016a18ef9261df87f7e205
Ryan Hass <ryan_hass@rapid7.com>
2015-03-04 13:11:21 -0800

    Loosen java cookbook depedency restriction.
    The current versions of the Java cookbook use a higher minor version
    number. This allows a newer version to be used.

--- metadata.rb
+++ metadata.rb
@@ -7,7 +7,7 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.3.0'
 
 depends 'ark',  '~> 0.4'
-depends 'java', '~> 1.13'
+depends 'java', '>= 1.13'
 depends 'windows'
 
 supports 'centos'
