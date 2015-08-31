c95d3659d6384e1d5ffb90098994b7fa8e05065b
John Bellone <jbellone@bloomberg.net>
2015-08-31 09:11:03 -0400

    Fixes foodcritic rule for using suggests keyword.

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,6 @@ description 'Application cookbook which installs and configures Maven.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '2.0.0'
 
-suggests 'java'
 depends 'libarchive', '~> 0.6'
 depends 'poise', '~> 2.2'
 depends 'rc', '~> 1.1'
