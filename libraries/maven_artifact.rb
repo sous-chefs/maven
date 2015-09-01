9da3ccaf4c29311c6074792353dd4b4115731fff
John Bellone <jbellone@bloomberg.net>
2015-09-01 14:51:25 -0400

    Fixes some more issues while testing regressions.

--- libraries/maven_artifact.rb
+++ libraries/maven_artifact.rb
@@ -17,20 +17,20 @@ module MavenCookbook
       provides(:maven_artifact)
 
       attribute(:artifact_id, kind_of: String, name_attribute: true)
+      attribute(:version, kind_of: String, required: true)
       attribute(:classifier, kind_of: String)
       attribute(:group_id, kind_of: String)
       attribute(:packaging, equal_to: %w{jar war pom}, default: 'jar')
       attribute(:repositories, kind_of: [String, Array])
-      attribute(:transitive, equal_to: [true, false], default: true)
-      attribute(:version, kind_of: String)
+      attribute(:transitive, equal_to: [true, false], default: false)
 
       attribute(:destination, kind_of: String)
       attribute(:owner, kind_of: String, default: 'root')
-      attribute(:group, kind_of: String)
+      attribute(:group, kind_of: String, default: 'root')
       attribute(:mode, kind_of: String, default: '0644')
 
       attribute(:plugin_version, kind_of: String, default: '2.10')
-      
+
       def friendly_basename
         [artifact_id, version, classifier].compact.join('-') + '.' + packaging
       end
@@ -39,9 +39,6 @@ module MavenCookbook
         repos = [new_resource.repositories].flatten.compact.join(',')
 
         notifying_block do
-          # If the destination directory is set on the resource we
-          # should attempt to create it with appropriate permissions &
-          # ownership.
           if new_resource.destination
             directory new_resource.destination do
               recursive true
@@ -69,16 +66,15 @@ module MavenCookbook
             directory new_resource.destination
             user new_resource.owner
             group new_resource.group
-            options do
-              version new_resource.version
-              artifactId new_resource.artifact_id
-              groupId new_resource.group_id
-              dest new_resource.destination
-              packaging new_resource.packaging
-              transitive new_resource.transitive
-              classifier new_resource.classifier
-              remoteRepositories repos if new_resource.repositories
-            end
+            options(
+              'version' => new_resource.version,
+              'artifactId' => new_resource.artifact_id,
+              'groupId' => new_resource.group_id,
+              'dest' => new_resource.destination,
+              'packaging' => new_resource.packaging,
+              'transitive' => new_resource.transitive,
+              'classifier' => new_resource.classifier,
+              'remoteRepositories' => new_resource.repositories ? repos : nil)
             notifies :run, "ruby_block[set permissions: #{new_resource.friendly_basename}]"
           end
         end
--- libraries/maven_execute.rb
+++ libraries/maven_execute.rb
@@ -17,7 +17,7 @@ module MavenCookbook
 
       attribute(:directory, kind_of: String, default: '/var/run/java')
       attribute(:user, kind_of: String, default: 'root')
-      attribute(:group, kind_of: String)
+      attribute(:group, kind_of: String, default: 'root')
 
       attribute(:command, kind_of: String, name_attribute: true)
       attribute(:environment, kind_of: Hash, default: lazy { default_environment })
@@ -33,13 +33,6 @@ module MavenCookbook
       action(:run) do
         options = new_resource.options.delete_if { |_, v| v.nil? }.map { |kv| '-D' + kv.join('=') }
         notifying_block do
-          directory new_resource.directory do
-            recursive true
-            owner new_resource.user
-            group new_resource.group
-            mode '0755'
-          end
-
           execute "mvn #{new_resource.command}" do # ~FC009
             command ['mvn', new_resource.command, options].flatten.map(&:strip).join(' ')
             user new_resource.user
