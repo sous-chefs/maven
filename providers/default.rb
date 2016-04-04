a8ce5181d20ce5179ac1621dd925ef40a259d64f
Tim Smith <tsmith84@gmail.com>
2016-04-04 10:20:46 -0700

    Resolve rubocop warning

--- providers/default.rb
+++ providers/default.rb
@@ -50,7 +50,7 @@ def get_mvn_artifact(action, new_resource)
 
   Dir.mktmpdir('chef_maven_lwrp') do |tmp_dir|
     tmp_file = ::File.join(tmp_dir, artifact_file_name)
-    shell_out!(create_command_string(tmp_file, new_resource), :timeout => new_resource.timeout)
+    shell_out!(create_command_string(tmp_file, new_resource), timeout: new_resource.timeout)
     dest_file = ::File.join(new_resource.dest, artifact_file_name)
 
     unless ::File.exist?(dest_file) && checksum(tmp_file) == checksum(dest_file)
