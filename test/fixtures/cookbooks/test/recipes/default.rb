9185481183465bdbb95f8eddfad498e07e67aecf
Tim Smith <tsmith@chef.io>
2016-08-29 16:20:32 -0700

    Use inspec and remove apt cookbook from testing
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- test/fixtures/cookbooks/test/recipes/default.rb
+++ test/fixtures/cookbooks/test/recipes/default.rb
@@ -1,6 +1,6 @@
-# make sure we have java installed
-include_recipe 'java'
+apt_update 'update' if platform_family?('debian')
 
+include_recipe 'java::default'
 include_recipe 'maven::default'
 
 user 'foobarbaz'
---
+++ test/integration/resource/resource_spec.rb
@@ -0,0 +1,33 @@
+describe file('/usr/local/foobar/lib/mysql-connector-java-5.1.19.jar') do
+  it { should be_a_file }
+  its('mode') { should eq 0755 }
+  it { should be_owned_by('foobarbaz') }
+end
+
+describe file('/usr/local/foobar/lib/javax.persistence-2.0.0.jar') do
+  it { should be_a_file }
+end
+
+describe file('/usr/local/foobar/lib/postgresql-9.0-801.jdbc4.jar') do
+  it { should be_a_file }
+end
+
+describe file('/usr/local/foobar/lib/mm-mysql-2.0.13.pom') do
+  it { should be_a_file }
+end
+
+describe file('/usr/local/foobar/lib/solr-foo.war') do
+  it { should be_a_file }
+end
+
+describe file('/etc/mavenrc') do
+  it { should be_a_file }
+end
+
+describe file('/usr/local/notifyOne') do
+  it { should be_a_file }
+end
+
+describe file('/usr/local/notifyTwo') do
+  it { should be_a_file }
+end
