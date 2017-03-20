2e22c9471dc834dfd6ac9f6b9d36621cc6e2465d
Tim Smith <tsmith@chef.io>
2017-03-20 12:59:01 -0700

    Test chef 13 compatibility in Travis
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'maven'
 maintainer       'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
-license          'Apache 2.0'
+license          'Apache-2.0'
 description      'Installs Apache Maven includes a resource for installing artifacts from maven'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '4.0.1'
