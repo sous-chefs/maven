79366d71d5729c126ea84f49417b7ae0b114930e
Tim Smith <tsmith@chef.io>
2017-04-26 16:07:13 -0700

    Convert default LWRP to a custom resource
    This could really use some additional refactoring, but testing does show it working
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -20,4 +20,4 @@ supports 'windows'
 
 source_url 'https://github.com/chef-cookbooks/maven'
 issues_url 'https://github.com/chef-cookbooks/maven/issues'
-chef_version '>= 12.1'
+chef_version '>= 12.5' if respond_to?(:chef_version)
