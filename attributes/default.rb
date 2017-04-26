208cf49ac7aa04c37d794336992aa28a551bd080
Tim Smith <tsmith@chef.io>
2017-04-26 16:07:55 -0700

    Use Maven 3.5.0 by default
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- attributes/default.rb
+++ attributes/default.rb
@@ -23,9 +23,9 @@
 
 default['maven']['m2_home'] = '/usr/local/maven'
 default['maven']['mavenrc']['opts'] = '-Dmaven.repo.local=$HOME/.m2/repository -Xmx384m'
-default['maven']['version'] = '3.3.9'
+default['maven']['version'] = '3.5.0'
 default['maven']['url'] = "http://apache.mirrors.tds.net/maven/maven-#{node['maven']['version'].split('.')[0]}/#{node['maven']['version']}/binaries/apache-maven-#{node['maven']['version']}-bin.tar.gz"
-default['maven']['checksum'] = '6e3e9c949ab4695a204f74038717aa7b2689b1be94875899ac1b3fe42800ff82'
+default['maven']['checksum'] = 'beb91419245395bd69a4a6edad5ca3ec1a8b64e41457672dc687c173a495f034'
 default['maven']['plugin_version'] = '2.10'
 default['maven']['repositories'] = ['http://repo1.maven.apache.org/maven']
 default['maven']['setup_bin'] = false
