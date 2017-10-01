8f641816a0871929df7733aec39aec6cb32a12aa
Jamie Tanna <jamie@jamietanna.co.uk>
2017-10-01 22:17:28 +0100

    Verify install_ark resource is correctly called
    Add tests to ensure that the correct parameters are passed into the
    `install_ark` resource.
    
    Note that we've overridden the default value of attributes for our Rspec
    test, in order to ensure that we're correctly passing in the attributes.
    
    As part of #82.
    
    Signed-off-by: Jamie Tanna <jamie@jamietanna.co.uk>

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -2,7 +2,13 @@ require 'spec_helper'
 
 describe 'default recipe' do
   cached(:chef_run) do
-    runner = ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '16.04')
+    runner = ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '16.04') do |node|
+      node.automatic['maven']['version'] = '1.2.3'
+      node.automatic['maven']['url'] = 'https://maven/maven.tar.gz'
+      node.automatic['maven']['checksum'] = '0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef'
+      node.automatic['maven']['m2_home'] = '/home/maven-user'
+      node.automatic['maven']['setup_bin'] = false
+    end
     runner.converge('maven::default')
   end
 
@@ -12,6 +18,12 @@ describe 'default recipe' do
 
   it 'downloads ark' do
     expect(chef_run).to install_ark('maven')
+      .with(version: '1.2.3')
+      .with(url: 'https://maven/maven.tar.gz')
+      .with(checksum: '0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef')
+      .with(home_dir: '/home/maven-user')
+      .with(win_install_dir: '/home/maven-user')
+      .with(append_env_path: false)
   end
 
   it 'writes the `/etc/mavenrc`' do
