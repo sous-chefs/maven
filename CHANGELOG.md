d57967102895365a295a9bc4af3e619ee030bd53
Tim Smith <tsmith@chef.io>
2017-11-01 09:37:07 -0700

    Release 5.0.2
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,10 @@
 
 This file is used to list changes made in each version of the maven cookbook.
 
+## 5.0.2 (2017-11-01)
+
+- Update the default maven URL to use the apache.org archive site since the binary has been moved off the main site
+
 ## 5.0.1 (2017-05-28)
 
 - Close settings.xml file after writing.
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'cookbooks@chef.io'
 license          'Apache-2.0'
 description      'Installs Apache Maven includes a resource for installing artifacts from maven'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '5.0.1'
+version          '5.0.2'
 
 depends 'ark',  '>= 1.0'
 depends 'java', '>= 1.38'
